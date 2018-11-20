f075f59c60491e6ddd991318c535b9b2498f09ae
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2018-11-20 23:01:35 +0900

    teacher question の API を編集

--- app/controllers/api/teachers/questions_controller.rb
+++ app/controllers/api/teachers/questions_controller.rb
@@ -1,10 +1,6 @@
 class Api::Teachers::QuestionsController < Api::Teachers::BaseController
   def index
     @questions = Question.where(problem_id: params[:problem_id])
-    if @questions.length.positive?
-      render :index, formats: :json, handlers: :jbuilder
-    else
-      not_found
-    end
+    render :index, formats: :json, handlers: :jbuilder
   end
 end
