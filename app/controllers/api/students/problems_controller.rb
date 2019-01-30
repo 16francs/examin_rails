30a6fcd4dac2c3def00260e8097481f887de46ff
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2019-01-31 02:30:11 +0900

    auto_loadのエラーの一時的な対処

--- app/controllers/api/students/problems_controller.rb
+++ app/controllers/api/students/problems_controller.rb
@@ -2,6 +2,9 @@
 
 class Api::Students::ProblemsController < Api::Students::BaseController
   def index
+    service = Students::ProblemsService.new
+    service.index
+    @response = service.response
     render json: {}, status: :ok
   end
 end
---
+++ app/services/students/problems_service.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Students::ProblemsService
+  attr_reader :response
+
+  def initialize
+    @response = nil
+  end
+
+  def index
+    @response = []
+
+    keys = %i[id title content user_id created_at updated_at]
+    problems = Problem.pluck(:id, :title, :content, :user_id, :created_at, :updated_at)
+    problems.map! { |problem| Hash[*[keys, problem].transpose.flatten] }
+
+    problems.map! do |problem|
+      user = User.find_by(id: problem[:user_id]) # user_id が nil OK なため
+      problem[:teacher_name] = user ? user[:name] : nil
+
+      problem.delete(:user_id)
+      problem
+    end
+
+    @response = problems
+  end
+end
