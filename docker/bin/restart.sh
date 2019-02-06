6aaceb3a56d0e0ca0d841b94944386b27cc23d8a
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2019-02-06 18:36:22 +0900

    docker の編集

--- docker/bin/restart.sh
+++ docker/bin/restart.sh
@@ -6,6 +6,9 @@ docker-compose down
 echo "--- docker-sync stop ---"
 docker-sync stop
 
+echo "--- rm server.pid ---"
+docker-compose run --rm web rm -f /examin_rails/tmp/pids/server.pid
+
 echo "--- docker-sync start ---"
 docker-sync start
 
--- docker/bin/start.sh
+++ docker/bin/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "--- rm server.pid ---"
+docker-compose run --rm web rm -f /examin_rails/tmp/pids/server.pid
+
 echo "--- docker-sync start ---"
 docker-sync start
 
