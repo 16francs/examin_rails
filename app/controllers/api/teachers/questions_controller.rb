c9b99b631318bd4694b3d138ed1ef7d73270c585
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2019-03-22 20:02:26 +0900

    teachers/questions関連の修正

--- app/controllers/api/teachers/questions_controller.rb
+++ app/controllers/api/teachers/questions_controller.rb
@@ -5,6 +5,6 @@ class Api::Teachers::QuestionsController < Api::Teachers::BaseController
     service = Teachers::QuestionsService.new
     service.index(params[:problem_id])
     @response = service.response
-    render :index, formats: :json, handlers: :jbuilder
+    render json: @response, status: :ok
   end
 end
--- app/services/teachers/questions_service.rb
+++ app/services/teachers/questions_service.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
-class Teachers::QuestionsService
-  attr_reader :response
-
-  def initialize
-    @response = nil
-  end
-
+class Teachers::QuestionsService < ApplicationService
   def index(problem_id)
     keys = %i[id sentence correct]
     questions = Question.where(problem_id: problem_id).pluck(:id, :sentence, :correct)
     questions.map! { |question| Hash[*[keys, question].transpose.flatten] }
 
-    @response = questions
+    @response[:questions] = questions
   end
 end
